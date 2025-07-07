b39395ae5481dfbe411df6d4b4eccfa42a3bd8ed
Devi Saranya Yanamadala <vvasuy78097@gmail.com>
2025-07-07 11:49:21 +0530

    Spring Boot application

---
+++ Week_3/SpringCore_Maven/LibraryManagement/src/main/java/com/library/LibraryManagement/controller/BookController.java
@@ -0,0 +1,42 @@
+package com.library.LibraryManagement.controller;
+
+
+import com.library.LibraryManagement.model.Book;
+import com.library.LibraryManagement.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+}
---
+++ Week_3/SpringCore_Maven/LibraryManagement/src/main/java/com/library/LibraryManagement/service/BookService.java
@@ -0,0 +1,43 @@
+package com.library.LibraryManagement.service;
+
+
+import com.library.LibraryManagement.model.Book;
+import com.library.LibraryManagement.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public Book updateBook(Long id, Book updatedBook) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            book.setTitle(updatedBook.getTitle());
+            book.setAuthor(updatedBook.getAuthor());
+            book.setYear(updatedBook.getYear());
+            return bookRepository.save(book);
+        }
+        return null;
+    }
+}
